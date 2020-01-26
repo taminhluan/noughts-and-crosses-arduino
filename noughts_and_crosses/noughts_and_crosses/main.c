0629a5997bef6a143535eba02bfa812bf3f82af3
Luan Ta <luantm@Luans-MacBook-Air.local>
2020-01-26 10:34:34 +0700

    create project with xcode

---
+++ noughts_and_crosses/noughts_and_crosses/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  noughts_and_crosses
+//
+//  Created by Luan Ta on 1/26/20.
+//  Copyright © 2020 Ta Minh Luan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Welcome to play noughts and crosses!\n");
+    return 0;
+}
